358836868f84bee8e28dc419ae84eed7715cb91a
Terrbox <terrbox.online@gmail.com>
2016-06-30 22:12:14 +0200

    Platzi course of reactjs

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+ * Module dependencies
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PokeTable from './components/PokeTable';
+
+var pokemons = [
+	{number: 1, name: 'Bulbasaur'},
+	{number: 2, name: 'Ivysaur'},
+	{number: 3, name: 'Venusaur'}
+];
+
+ReactDOM.render(<PokeTable pokemons={pokemons} />, document.getElementById('container'));
